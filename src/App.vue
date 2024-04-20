24e99846132e41cf3efe7c7bcc4c555c62af3918
loveWSA <3473443855@qq.com>
2024-04-21 00:08:02 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(1)
+const add = () => {
+  count.value++
+}
+const sub = () => {
+  count.value--
+}
+</script>
+
+<template>
+  <div>
+    {{ count }}
+    <button @click="add">+</button>
+    <button @click="sub">-</button>
+  </div>
+</template>
