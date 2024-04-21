f2ae2c32b429978dda730c2bb1264b39487fe385
loveWSA <3473443855@qq.com>
2024-04-21 16:52:17 +0800

    pinia统一管理

---
+++ src/stores/modules/user.js
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+// 用户模板 token setToken removeToken
+export const useUserStore = defineStore(
+  'user',
+  () => {
+    const token = ref('')
+    const setToken = (newToken) => {
+      token.value = newToken
+    }
+    const removeToken = () => {
+      token.value = ''
+    }
+    return {
+      token,
+      setToken,
+      removeToken
+    }
+  },
+  {
+    persist: true
+  }
+)
